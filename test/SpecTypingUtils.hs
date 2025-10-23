4dc4aacc789f3acafba43e9932fb9ca6c3e62c0b
arthur100500 <arthur100500@gmail.com>
2025-10-23 23:29:25 +0300

    [feat] Setup minimal tests
    Signed-off-by: arthur100500 <arthur100500@gmail.com>

--- test/SpecTypingUtils.hs
+++ test/SpecTypingUtils.hs
@@ -1,8 +1,15 @@
 module SpecTypingUtils (tests) where
 
+import Data.Function
 import Test.Run
 import Test.HUnit
+import Test.QuickCheck qualified as QC
+import Test.QuickCheck ( (==>) )
+import Test.QuickCheck.Classes.Base qualified as QC
 import TypingUtils
+import Types
+import Arbitrary.MonoTypes
+
 
 tests :: NamedTests
 tests = 
@@ -10,13 +17,14 @@ tests =
   , ("LUB Laws", testLubLaws)
   ]
 
-
-
 testGlbLaws :: Test
-testGlbLaws = TestLabel "glb laws" $ TestList
-  [ ]
-
+testGlbLaws = TestList
+ [ propertyToTest "forall x => glb(x, x) = x" \(x :: MonoTy) -> let ?tyCtx = [] in x `glb` x == x
+ , propertyToTest "x <: y => glb(x, y) = x" \(Subtyped (x, y)) -> let ?tyCtx = [] in x `glb` y == x
+ ]
 
 testLubLaws :: Test
-testLubLaws = TestLabel "дги laws" $ TestList
-  [ ]
+testLubLaws = TestList
+ [ propertyToTest "x <: y => lub(x, y) = y" \(Subtyped (x, y)) -> let ?tyCtx = [] in x `lub` y == y
+ , propertyToTest "forall x => lub(x, x) = x" \(x :: MonoTy) -> let ?tyCtx = [] in x `lub` x == x
+ ]
--- test/Test/Run.hs
+++ test/Test/Run.hs
@@ -1,11 +1,28 @@
 module Test.Run where
 
+import Control.Exception
 import Test.HUnit.Base qualified as HU
 import Test.QuickCheck qualified as QC
+import Test.QuickCheck.Classes.Base qualified as QC
 import Control.Monad (forM_)
 import Text.Printf (printf)
+import Test.QuickCheck (Result(output))
 
 type NamedTests = [(String, HU.Test)]
+
+propertyToTest :: QC.Testable prop => String -> prop -> HU.Test
+propertyToTest description = propertyToTestIO description . pure
+
+propertyToTestIO :: QC.Testable prop => String -> IO prop -> HU.Test
+propertyToTestIO description property = HU.TestCase $
+  property >>= QC.quickCheckWithResult args >>= \case
+    QC.Failure { theException = Just exception } ->
+      HU.assertFailure $ "tested " <> description <> ", but\n" <> show exception
+    QC.Failure { theException = Nothing, output } ->
+      HU.assertFailure $ "tested " <> description <> ", but\n" <> output
+    _ -> pure ()
+  where
+    args = QC.stdArgs { QC.chatty = True, QC.maxSuccess = 150, QC.maxSize = 2 }
 
 testMain :: NamedTests -> IO ()
 testMain tests = do
@@ -14,7 +31,7 @@ testMain tests = do
     runTest (name, test) = do
       printf "Running: %s\n" name
       (counts, _) <- HU.performTest reportStart reportError reportFailure () test
-      printf "  Ok: %d/%d\n" (HU.failures counts) (HU.cases counts)
+      printf "  Ok: %d/%d\n" (HU.cases counts - HU.failures counts) (HU.cases counts)
     reportStart _ _ = pure ()
     reportError _ msg HU.State{} () = printf "  Err: %s\n" msg
     reportFailure _ msg HU.State{} () = printf "  Fail: %s\n" msg
